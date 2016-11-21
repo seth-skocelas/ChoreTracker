0919d7d3d4e67ccf8b00d0fafc340b43006af87c
seth-skocelas <s.skocelas@gmail.com>
2016-11-20 22:35:42 -0600

    Initial Commit

---
+++ ChoreTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChoreTracker
+//
+//  Created by Seth Skocelas on 11/20/16.
+//  Copyright © 2016 Seth Skocelas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
